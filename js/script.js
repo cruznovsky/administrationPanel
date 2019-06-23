5f8a22ceabe3c91be4dc0e404af83957eee5afe7
Jakub Sadowski <jaakub.sadowski@gmail.com>
2019-06-23 16:45:00 +0200

    Add structure of NewLink modal

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Funkcja otwierająca modal
+function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function (modal) {
+        modal.classList.remove('show')
+    })
+    document.querySelector('#overlay').classList.add('show')
+    document.querySelector(modal).classList.add('show')
+}
+// Funkcja zamykająca modal
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+}
+// Zamknięcie modala poprzez X lub guzik na dole
+document.querySelectorAll('#overlay .js--close').forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        e.preventDefault()
+        closeModal()
+    })
+})
+// Zamknięcie modala poprzez kliknięcie na ciemne tło
+document.querySelector('#overlay').addEventListener('click', function (e) {
+    if (e.target === this) {
+        closeModal()
+    }
+})
+// Zamknięcie modala poprzez kliknięcie ESC
+document.addEventListener('keyup', function (e) {
+    if (e.keyCode === 27) {
+        closeModal()
+    }
+})
+
+var newLink = document.getElementsByClassName('addNewLink');
+
+newLink[0].addEventListener('click', function () {
+    openModal('#newLink');
+});
+newLink[1].addEventListener('click', function () {
+    openModal('#newLink');
+});
