30ddf0ac58f9856ded355ddb62d8cbefd9c7d19c
Jakub Sadowski <jaakub.sadowski@gmail.com>
2019-07-01 21:27:00 +0200

    Add hover effects

--- js/script.js
+++ js/script.js
@@ -61,3 +61,102 @@ login.addEventListener('click', function () {
 exit.addEventListener('click', function () {
     openModal('#exit');
 });
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+    // 1
+    type: 'bar',
+    data: {
+        // 2
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        // 3
+        datasets: [{
+            // 4
+            label: "Signups",
+            // 5
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            // 6
+            data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+            // 7
+            hidden: true,
+        }]
+    },
+});
+
+var generalCard = document.getElementById('general');
+var detailsCard = document.getElementById('details');
+var linksCard = document.getElementById('links');
+var bannerCard = document.getElementById('banner');
+var personalCard = document.getElementById('personal');
+var payoutCard = document.getElementById('payout');
+var postbackCard = document.getElementById('postback');
+
+var cards = [
+    generalCard, detailsCard, linksCard, bannerCard, personalCard, payoutCard, postbackCard
+]
+
+
+var generalMenu = document.getElementById('generalMenu');
+var detailsMenu = document.getElementById('detailsMenu');
+var linksMenu = document.getElementById('linksMenu');
+var bannerMenu = document.getElementById('bannerMenu');
+var personalMenu = document.getElementById('personalMenu');
+var payoutMenu = document.getElementById('payoutMenu');
+var postbackMenu = document.getElementById('postbackMenu');
+
+
+var test = document.querySelectorAll('hider');
+var testowo = document.querySelectorAll('leftNavbar__menu--item-link');
+
+// detailsMenu.addEventListener('click', function () {
+//     generalCard.classList.add('cardHide');
+//     detailsCard.classList.remove('cardHide');
+// })
+
+detailsMenu.addEventListener('click', function () {
+    toogleVisibility(detailsCard)
+})
+
+generalMenu.addEventListener('click', function () {
+    toogleVisibility(generalCard);
+})
+
+linksMenu.addEventListener('click', function () {
+    toogleVisibility(linksCard)
+})
+bannerMenu.addEventListener('click', function () {
+    toogleVisibility(bannerCard)
+})
+personalMenu.addEventListener('click', function () {
+    toogleVisibility(personalCard)
+})
+payoutMenu.addEventListener('click', function () {
+    toogleVisibility(pauoutCard)
+})
+postbackMenu.addEventListener('click', function () {
+    toogleVisibility(postbackCard)
+})
+
+
+
+function toogleVisibility(visibleCard) {
+    cards.forEach(function (card) {
+        card.classList.add('cardHide');
+    })
+    visibleCard.classList.remove('cardHide');
+}
+
